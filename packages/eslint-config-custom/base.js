6a1b3dd34a176cde924c6230d1503ccf58e04d3e
Dongho Kim <dongre2527@gmail.com>
2023-07-17 16:42:39 +0900

    fix eslint issue

--- packages/eslint-config-custom/base.js
+++ packages/eslint-config-custom/base.js
@@ -1,11 +1,15 @@
 // https://typescript-eslint.io/getting-started
 module.exports = {
   extends: [
-    "prettier",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    'prettier',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    files: ['*.ts'],
+  },
 };
--- packages/eslint-config-custom/react.js
+++ packages/eslint-config-custom/react.js
@@ -1,19 +1,31 @@
 module.exports = {
   extends: [
-    "./base.js",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
+    './base.js',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
   ],
   env: {
     browser: true,
     es2020: true,
   },
-  plugins: ["react-refresh"],
+  plugins: ['react-refresh'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
-    "@typescript-eslint/no-non-null-assertion": "off",
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+    },
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
