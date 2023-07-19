4c9b67d22f3040540d5654eeb7469b803a0d791f
Dongho Kim <dongre2527@gmail.com>
2023-07-19 16:06:52 +0900

    add disabled styles to button component

--- apps/frontend/src/components/button.tsx
+++ apps/frontend/src/components/button.tsx
@@ -20,12 +20,13 @@ export function Button({
       className={`py-1 px-4 rounded-full ${
         {
           primary: {
-            white: 'bg-white text-black',
-            red: 'bg-red-500 text-red-500',
+            white: 'bg-white text-black disabled:bg-neutral-500',
+            red: 'bg-red-500 text-white disabled:bg-neutral-500 disabled:text-black',
           },
           secondary: {
-            white: 'text-white border border-white',
-            red: 'text-red-500 border border-red-500',
+            white:
+              'text-white border border-white disabled:text-neutral-500 disabled:border-neutral-500',
+            red: 'text-red-500 border border-red-500 disabled:text-neutral-500 disabled:border-neutral-500',
           },
         }[variant][color]
       }`}
