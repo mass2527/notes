8fdbe474608c1e16b1748ce91f504508b1735037
Dongho Kim <dongre2527@gmail.com>
2023-07-19 15:56:55 +0900

    handle note list loading, error, empty cases

--- apps/frontend/src/routes/root/note-list.tsx
+++ apps/frontend/src/routes/root/note-list.tsx
@@ -7,24 +7,42 @@ export default function NoteList() {
   const currentUser = useCurrentUser();
   const notesQueryResult = useNotesQuery(currentUser.id);
 
-  return (
-    <ul className="flex flex-col gap-4">
-      {notesQueryResult.data?.map((note) => (
-        <li key={note.id}>
-          <Link
-            to={`/notes/${note.id}`}
-            className="flex flex-col p-4 border border-neutral-700 rounded-lg"
-          >
-            <strong>{note.title}</strong>
-            <time
-              dateTime={note.updatedAt}
-              className="text-sm text-neutral-500"
-            >
-              {getFormattedDate(new Date(note.updatedAt))}
-            </time>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
+  if (notesQueryResult.data) {
+    return (
+      <ul className="flex flex-col gap-4">
+        {notesQueryResult.data.length > 0 ? (
+          notesQueryResult.data.map((note) => (
+            <li key={note.id}>
+              <Link
+                to={`/notes/${note.id}`}
+                className="flex flex-col p-4 border border-neutral-700 rounded-lg"
+              >
+                <strong>{note.title}</strong>
+                <time
+                  dateTime={note.updatedAt}
+                  className="text-sm text-neutral-500"
+                >
+                  {getFormattedDate(new Date(note.updatedAt))}
+                </time>
+              </Link>
+            </li>
+          ))
+        ) : (
+          <p>
+            <strong>
+              Click the <span className="underline">NEW</span> link
+            </strong>
+            <br />
+            to create your first note!
+          </p>
+        )}
+      </ul>
+    );
+  }
+
+  if (notesQueryResult.error) {
+    return <div>Error...</div>;
+  }
+
+  return <div>Loading...</div>;
 }
