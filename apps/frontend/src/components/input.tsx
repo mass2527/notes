b4b82dac598ac7dead042b7ea8817add5facaeba
Dongho Kim <dongre2527@gmail.com>
2023-07-20 14:49:03 +0900

    change input, textarea disabled style

--- apps/frontend/src/components/input.tsx
+++ apps/frontend/src/components/input.tsx
@@ -6,7 +6,9 @@ const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
   return (
     <input
       ref={ref}
-      className="py-1 px-4 rounded-[6px] bg-transparent border border-neutral-700 placeholder:text-neutral-500 hover:border-white focus-within:border-white outline-none"
+      className={`py-1 px-4 rounded-[6px] 
+      bg-transparent placeholder:text-neutral-500 
+      border border-neutral-700 hover:border-white disabled:border-neutral-700`}
       {...props}
     />
   );
--- apps/frontend/src/components/textarea.tsx
+++ apps/frontend/src/components/textarea.tsx
@@ -7,7 +7,9 @@ function Textarea({ className, ...props }: Props) {
   return (
     <textarea
       className={twMerge(
-        'py-1 px-4 rounded-[6px] bg-transparent border border-neutral-700 placeholder:text-neutral-500 hover:border-white focus-within:border-white outline-none',
+        `py-1 px-4 rounded-[6px] 
+        bg-transparent placeholder:text-neutral-500
+        border border-neutral-700 hover:border-white disabled:border-neutral-700`,
         className,
       )}
       {...props}
