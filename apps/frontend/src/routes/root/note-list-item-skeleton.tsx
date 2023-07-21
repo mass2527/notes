a16158cce4ca55fc1cef9f7713fe6ff8b25b9c50
Dongho Kim <dongre2527@gmail.com>
2023-07-21 12:07:25 +0900

    fix note list item skeleton's inaccurate size

--- apps/frontend/src/routes/root/note-list-item-skeleton.tsx
+++ apps/frontend/src/routes/root/note-list-item-skeleton.tsx
@@ -1,12 +1,12 @@
+import TextSkeleton from '../../components/text-skeleton';
+
 function NoteListItemSkeleton() {
   return (
     <div className="p-4 border border-neutral-700 rounded-lg">
       <div className="animate-pulse">
-        <div className="space-y-3">
-          <div className="h-4 bg-neutral-700 rounded-sm"></div>
-          <div className="grid grid-cols-3">
-            <div className="h-4 bg-neutral-700 rounded-sm col-span-1"></div>
-          </div>
+        <TextSkeleton fontSize={16} />
+        <div className="grid grid-cols-3">
+          <TextSkeleton fontSize={14} lineHeight={20} />
         </div>
       </div>
     </div>
