cf94b990a305d316a46461dc4247fbc4487b471f
Dongho Kim <dongre2527@gmail.com>
2023-07-22 21:28:56 +0900

    style TextWithMarkdown component

--- apps/frontend/src/components/text-with-markdown.tsx
+++ apps/frontend/src/components/text-with-markdown.tsx
@@ -3,7 +3,8 @@ import DOMPurify from 'dompurify';
 
 function TextWithMarkdown({ text }: { text: string }) {
   return (
-    <div
+    <article
+      className="prose prose-neutral dark:prose-invert prose-img:rounded prose-a:text-blue-500"
       dangerouslySetInnerHTML={{
         __html: DOMPurify.sanitize(marked.parse(text)),
       }}
