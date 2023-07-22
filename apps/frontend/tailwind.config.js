3896897f3a92320a5eb9c74d4cbbe5b13a49da91
Dongho Kim <dongre2527@gmail.com>
2023-07-22 14:09:36 +0900

    fix the unapplied tailwindcss of react package's components

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    // https://github.com/vercel/turbo/blob/main/examples/with-tailwind/README.md#building-packagesui
+    '../../packages/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
