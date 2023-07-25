a84fba202f36d8603f28e2c8d52b100010d6c5c4
Dongho Kim <dongre2527@gmail.com>
2023-07-25 14:13:44 +0900

    update cors origin

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -7,16 +7,13 @@ const prisma = new PrismaClient();
 const fastify = Fastify({
   logger: false,
 });
-try {
-  await fastify.register(cors, {
-    origin: '*',
-  });
-} catch (error) {
-  console.error(error);
-}
 
-fastify.get('/', () => {
-  return 'working';
+await fastify.register(cors, {
+  origin: /^http:\/\/localhost:\d{4}$/,
+});
+
+fastify.get('/', (req, res) => {
+  return res.send('reply');
 });
 
 fastify.get(
