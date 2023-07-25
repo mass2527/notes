a3da7dc7db35e935e295a45e17b720aab5625485
Dongho Kim <dongre2527@gmail.com>
2023-07-26 00:35:50 +0900

    add motion-safe:

--- apps/frontend/src/routes/root/note-list-item-skeleton.tsx
+++ apps/frontend/src/routes/root/note-list-item-skeleton.tsx
@@ -3,7 +3,7 @@ import { TextSkeleton } from '@philly/react';
 function NoteListItemSkeleton() {
   return (
     <div className="p-4 border border-neutral-700 rounded-lg">
-      <div className="animate-pulse">
+      <div className="motion-safe:animate-pulse">
         <TextSkeleton fontSize={16} />
         <div className="grid grid-cols-3">
           <TextSkeleton fontSize={14} lineHeight={20} />
