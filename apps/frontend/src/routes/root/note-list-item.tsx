988fbe688abe181730abf33177afc79ade11dc94
Dongho Kim <dongre2527@gmail.com>
2023-07-25 23:36:05 +0900

    change note list styles based on navigation status

--- apps/frontend/src/routes/root/note-list-item.tsx
+++ apps/frontend/src/routes/root/note-list-item.tsx
@@ -1,19 +1,22 @@
-import { LinkWithQuery } from '@philly/react';
 import { Note } from '../../types';
 import { getFormattedDate } from '../../utils/time';
+import { NavLink } from 'react-router-dom';
 
 function NoteListItem({ note }: { note: Note }) {
   return (
     <li>
-      <LinkWithQuery
+      <NavLink
         to={`/notes/${note.id}`}
-        className="flex flex-col p-4 border border-neutral-700 rounded-lg"
+        className={({ isActive, isPending }) =>
+          `flex flex-col p-4 border border-neutral-700 rounded-lg hover:bg-neutral-900 motion-safe:transition-colors motion-safe:ease-in-out motion-safe:duration-150
+          ${isPending ? 'text-neutral-500' : isActive ? 'border-white' : ''}`
+        }
       >
         <strong>{note.title}</strong>
         <time dateTime={note.updatedAt} className="text-sm text-neutral-500">
           {getFormattedDate(new Date(note.updatedAt))}
         </time>
-      </LinkWithQuery>
+      </NavLink>
     </li>
   );
 }
