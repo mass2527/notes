35d7b887251166e41c1af5a130bb73f257d412c2
Dongho Kim <dongre2527@gmail.com>
2023-07-26 13:56:38 +0900

    refactor NotesQueryInput using Form

--- apps/frontend/src/routes/root/notes-query-input.tsx
+++ apps/frontend/src/routes/root/notes-query-input.tsx
@@ -1,9 +1,9 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import invariant from 'tiny-invariant';
 import { useDocumentKeydownEventListener } from '../../hooks/use-document-key-down-event-listener';
 import { isWithPlatformMetaKey } from '../../utils/platform';
 import { Input } from '@philly/react';
-import { Form, useLoaderData, useSubmit } from 'react-router-dom';
+import { Form, useLoaderData, useLocation, useSubmit } from 'react-router-dom';
 import { UnwrapLoader } from '../../utils/types';
 import { rootLoader } from './loader';
 
@@ -11,6 +11,14 @@ function NotesQueryInput() {
   const inputRef = useRef<HTMLInputElement>(null);
   const { q } = useLoaderData() as UnwrapLoader<typeof rootLoader>;
   const submit = useSubmit();
+  const location = useLocation();
+
+  useEffect(() => {
+    const inputElement = inputRef.current;
+    invariant(inputElement);
+
+    inputElement.value = q;
+  }, [q]);
 
   useDocumentKeydownEventListener((event) => {
     const inputElement = inputRef.current;
@@ -26,7 +34,7 @@ function NotesQueryInput() {
   });
 
   return (
-    <Form role="search">
+    <Form role="search" action={location.pathname}>
       <Input
         type="search"
         name="q"
@@ -36,7 +44,10 @@ function NotesQueryInput() {
         maxLength={64}
         defaultValue={q}
         onChange={(event) => {
-          submit(event.currentTarget.form);
+          const isFirstSearch = q === '';
+          submit(event.currentTarget.form, {
+            replace: !isFirstSearch,
+          });
         }}
       />
     </Form>
