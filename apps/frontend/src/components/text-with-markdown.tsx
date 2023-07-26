d77fb13d0c078406ec2c3bce761642b85273351b
Dongho Kim <dongre2527@gmail.com>
2023-07-26 11:23:02 +0900

    add TextWithMarkdown code block styles

--- apps/frontend/src/components/text-with-markdown.tsx
+++ apps/frontend/src/components/text-with-markdown.tsx
@@ -4,7 +4,7 @@ import DOMPurify from 'dompurify';
 function TextWithMarkdown({ text }: { text: string }) {
   return (
     <article
-      className="prose prose-neutral dark:prose-invert prose-img:rounded prose-a:text-blue-500"
+      className="prose prose-neutral dark:prose-invert prose-img:rounded prose-a:text-blue-500 prose-pre:border prose-pre:border-neutral-700 prose-pre:bg-neutral-900 prose-a:no-underline hover:prose-a:underline"
       dangerouslySetInnerHTML={{
         __html: DOMPurify.sanitize(marked.parse(text)),
       }}
