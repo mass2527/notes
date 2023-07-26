aeb2e32aca78b723973bf212ff03fb63a52513b6
Dongho Kim <dongre2527@gmail.com>
2023-07-26 12:05:45 +0900

    add loading css

--- apps/frontend/src/routes/root/index.tsx
+++ apps/frontend/src/routes/root/index.tsx
@@ -1,12 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Sidebar from './sidebar';
 
 function Root() {
+  const { state } = useNavigation();
+
   return (
     <div className="flex h-screen">
       <Sidebar />
 
-      <main className="flex-1 p-16">
+      <main
+        className={`flex-1 p-16 ${state === 'loading' ? 'opacity-50' : ''}`}
+      >
         <Outlet />
       </main>
     </div>
