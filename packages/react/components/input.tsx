dc106f0c7b57e8efde9297fc987508cc771479e7
Dongho Kim <dongre2527@gmail.com>
2023-07-26 15:42:40 +0900

    add label to Input component

--- packages/react/components/input.tsx
+++ packages/react/components/input.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes, forwardRef } from 'react';
+import { InputHTMLAttributes, forwardRef, useId } from 'react';
 import { LoadingIcon, SearchIcon } from './icons';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
@@ -7,14 +7,19 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {
 
 export const Input = forwardRef<HTMLInputElement, Props>(
   ({ isLoading, type, ...props }, ref) => {
+    const id = useId();
+
     return (
       <div className="flex items-center gap-2 py-1 px-4 rounded-[6px] border border-neutral-700 hover:border-white disabled:border-neutral-700 focus-within:ring-4">
         {isLoading ? (
           <LoadingIcon />
         ) : type === 'search' ? (
-          <SearchIcon className="text-neutral-500 w-4 h-4" />
+          <label htmlFor={id}>
+            <SearchIcon className="text-neutral-500 w-4 h-4" />
+          </label>
         ) : null}
         <input
+          id={id}
           type={type}
           ref={ref}
           className={`placeholder:text-neutral-500 bg-transparent ring-0`}
