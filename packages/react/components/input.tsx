c207264a99f2c6bac7213fa86c17cca3420fbdce
Dongho Kim <dongre2527@gmail.com>
2023-07-26 13:34:31 +0900

    add isLoading prop, SearchIcon to Input component

--- packages/react/components/input.tsx
+++ packages/react/components/input.tsx
@@ -1,15 +1,27 @@
 import { InputHTMLAttributes, forwardRef } from 'react';
+import { LoadingIcon, SearchIcon } from './icons';
 
-interface Props extends InputHTMLAttributes<HTMLInputElement> {}
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  isLoading?: boolean;
+}
 
-export const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
-  return (
-    <input
-      ref={ref}
-      className={`py-1 px-4 rounded-[6px] 
-      bg-transparent placeholder:text-neutral-500 
-      border border-neutral-700 hover:border-white disabled:border-neutral-700`}
-      {...props}
-    />
-  );
-});
+export const Input = forwardRef<HTMLInputElement, Props>(
+  ({ isLoading, type, ...props }, ref) => {
+    return (
+      <div className="flex items-center gap-2 py-1 px-4 rounded-[6px] border border-neutral-700 hover:border-white disabled:border-neutral-700 focus-within:ring-4">
+        {isLoading ? (
+          <LoadingIcon />
+        ) : type === 'search' ? (
+          <SearchIcon className="text-neutral-500 w-4 h-4" />
+        ) : null}
+        <input
+          type={type}
+          ref={ref}
+          className={`placeholder:text-neutral-500 bg-transparent ring-0`}
+          autoComplete="off"
+          {...props}
+        />
+      </div>
+    );
+  },
+);
