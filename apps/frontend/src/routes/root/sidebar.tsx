04bb8edd4adb009c5b9795aafcc1d838b5d13576
Dongho Kim <dongre2527@gmail.com>
2023-07-26 22:55:10 +0900

    reduce Notes link click area

--- apps/frontend/src/routes/root/sidebar.tsx
+++ apps/frontend/src/routes/root/sidebar.tsx
@@ -6,9 +6,11 @@ import NotesQueryInput from './notes-query-input';
 function Sidebar() {
   return (
     <div className="flex flex-col p-4 border-r border-neutral-700 w-[300px]">
-      <LinkWithQuery className="text-2xl font-medium" to="/">
-        Notes
-      </LinkWithQuery>
+      <div>
+        <LinkWithQuery className="text-2xl font-medium" to="/">
+          Notes
+        </LinkWithQuery>
+      </div>
 
       <Spacing size={16} />
 
