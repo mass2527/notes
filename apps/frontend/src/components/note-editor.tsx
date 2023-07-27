7e44b148f896fc34b6e03935b13fd7a3a7c3cf41
Dongho Kim <dongre2527@gmail.com>
2023-07-27 13:57:13 +0900

    refactor NoteEditor

--- apps/frontend/src/components/note-editor.tsx
+++ apps/frontend/src/components/note-editor.tsx
@@ -3,55 +3,55 @@ import { NoteForm } from '../types';
 import { twMerge } from 'tailwind-merge';
 import { Input, Textarea } from '@philly/react';
 
-function NoteEditor(
-  props: (
-    | {
-        note: NoteForm;
-        setNote: React.Dispatch<React.SetStateAction<NoteForm>>;
-        onKeyDown?: KeyboardEventHandler<
-          HTMLInputElement | HTMLTextAreaElement
-        >;
-        disabled: false;
-      }
-    | {
-        disabled: true;
-      }
-  ) & { className?: string; header?: ReactNode },
-) {
+function NoteEditor({
+  note,
+  setNote,
+  onKeyDown,
+  disabled,
+  className,
+  header,
+}: {
+  note?: NoteForm;
+  setNote?: React.Dispatch<React.SetStateAction<NoteForm>>;
+  onKeyDown?: KeyboardEventHandler<HTMLInputElement | HTMLTextAreaElement>;
+  disabled?: boolean;
+  className?: string;
+  header?: ReactNode;
+}) {
   const changeNoteFields = (
     event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => {
-    if (!props.disabled) {
-      props.setNote({
-        ...props.note,
+    if (setNote) {
+      setNote((prev) => ({
+        ...prev,
         [event.target.name]: event.target.value,
-      });
+      }));
     }
   };
 
   return (
-    <div className={twMerge('h-full flex flex-col gap-4', props.className)}>
-      {props.header}
+    <div className={twMerge('h-full flex flex-col gap-4', className)}>
+      {header}
       <div className="flex flex-col gap-4 h-full">
         <Input
           name="title"
-          value={props.disabled ? '' : props.note.title}
+          value={note?.title}
           onChange={changeNoteFields}
           placeholder="Title"
           autoFocus
-          onKeyDown={props.disabled ? undefined : props.onKeyDown}
+          onKeyDown={onKeyDown}
           maxLength={100}
-          disabled={props.disabled}
+          disabled={disabled}
         />
         <Textarea
           name="content"
-          value={props.disabled ? '' : props.note.content}
+          value={note?.content}
           onChange={changeNoteFields}
           className="h-full resize-none"
           placeholder="Content"
-          onKeyDown={props.disabled ? undefined : props.onKeyDown}
+          onKeyDown={onKeyDown}
           maxLength={3000}
-          disabled={props.disabled}
+          disabled={disabled}
         />
       </div>
     </div>
