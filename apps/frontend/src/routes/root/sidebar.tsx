ae90dd574cd7f5cf95182f807a0c0321bfcb48e0
Dongho Kim <dongre2527@gmail.com>
2023-07-27 14:03:42 +0900

    move NewNoteButton

--- apps/frontend/src/routes/root/sidebar.tsx
+++ apps/frontend/src/routes/root/sidebar.tsx
@@ -5,18 +5,18 @@ import NotesQueryInput from './notes-query-input';
 
 function Sidebar() {
   return (
-    <div className="flex flex-col p-4 border-r border-neutral-700 w-[300px]">
-      <div>
+    <div className="flex flex-col p-4 border-r border-neutral-700 w-[200px]">
+      <div className="flex justify-between">
         <LinkWithQuery className="text-2xl font-medium" to="/">
           Notes
         </LinkWithQuery>
+        <NewNoteButton />
       </div>
 
       <Spacing size={16} />
 
       <div className="flex items-center gap-2">
         <NotesQueryInput />
-        <NewNoteButton />
       </div>
       <Spacing size={16} />
 
