2e43d1f64522b5aa12ca911e7360835a71ae1b51
Dongho Kim <dongre2527@gmail.com>
2023-07-27 14:16:34 +0900

    change sidebar width

--- apps/frontend/src/routes/root/sidebar.tsx
+++ apps/frontend/src/routes/root/sidebar.tsx
@@ -5,7 +5,7 @@ import NotesQueryInput from './notes-query-input';
 
 function Sidebar() {
   return (
-    <div className="flex flex-col p-4 border-r border-neutral-700 w-[200px]">
+    <div className="flex flex-col p-4 border-r border-neutral-700 w-[250px]">
       <div className="flex justify-between">
         <LinkWithQuery className="text-2xl font-medium" to="/">
           Notes
